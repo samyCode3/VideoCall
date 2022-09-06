73bd87ba033d1e66d7128fe1b925cdb4e6fa5a47
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-09-06 07:45:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app =  express()
 const server = require('http').Server(app)
 const io =require('socket.io')(server)
+const PORT = process.env.PORT || 3000
 const { v4: uuid } = require('uuid')
 
 app.set("view engine", "ejs")
@@ -30,6 +31,6 @@ io.on('connection', socket => {
    })
 })
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
    console.log("Server is started at 3000")
 })
