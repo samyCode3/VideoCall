18d52715b095ffa7f298e1a42b3e594458b4066d
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-09-06 11:45:07 +0100

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'))
 app.get("/", (req, res) => {
    res.redirect(`/${uuid()}`)
 })
-app.get("/:index", (req, res) => {
+app.get("/:room", (req, res) => {
 
    res.render('room', {roomId: req.params.room })
 })
